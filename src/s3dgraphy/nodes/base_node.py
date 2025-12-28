74fc5bc25f486317386edf9ce01d3ea583bd2a41
Emanuel Demetrescu <emanuel.demetrescu@gmail.com>
2025-12-28 17:45:46 +0100

    code cleaned removing old versions

--- src/s3dgraphy/nodes/base_node.py
+++ src/s3dgraphy/nodes/base_node.py
@@ -1,32 +1,8 @@
 # 3dgraphy/nodes/base_node.py
 
-import json
-import os
-
-def load_json_mapping(filename):
-    """
-    Load JSON mapping data from a specified file.
-
-    Args:
-        filename (str): Name of the JSON file containing mapping data.
-
-    Returns:
-        dict: Mapping data loaded from the JSON file.
-    """
-    # Construct the absolute path
-    mapping_path = os.path.join(os.path.dirname(__file__), '..', 'JSON_config', filename)
-    mapping_path = os.path.abspath(mapping_path)
-
-    try:
-        with open(mapping_path, 'r') as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        print(f"Error loading mapping file: {e}")
-        return {}
-
 class Node:
     """
-    Base class to represent a node in the graph with CIDOC mapping.
+    Base class to represent a node in the graph.
 
     Attributes:
         node_id (str): Unique identifier for the node.
@@ -34,7 +10,6 @@ class Node:
         node_type (str): Type of the node.
         description (str): Description of the node.
         attributes (dict): Dictionary for additional attributes.
-        mapping (dict): CIDOC mapping data specific to the node type.
     """
     node_type = "Node"  # Attributo di classe
     node_type_map = {}  # Mappatura tra node_type e classi
@@ -51,17 +26,6 @@ class Node:
         self.description = description
         self.node_type = self.__class__.node_type
         self.attributes = {}
-        self.mapping = self.load_mapping()
 
     def add_attribute(self, key, value):
         self.attributes[key] = value
-
-    def load_mapping(self):
-        """
-        Loads the CIDOC mapping specific to the node type.
-
-        Returns:
-            dict: CIDOC mapping data if found, else an empty dictionary.
-        """
-        mappings = load_json_mapping('em_cidoc_mapping.json')
-        return mappings.get(self.node_type, {})
